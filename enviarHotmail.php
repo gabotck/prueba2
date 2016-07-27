9bc900107e98c9869ea353abaca1db57605a1f67
gabotck <gabotck@hotmail.com>
2016-07-27 17:32:30 -0500

    v1.0
    asd

---
+++ enviarHotmail.php
@@ -0,0 +1,11 @@
+<?php
+$para      = 'gabotck@hotmail.com';
+$titulo = 'El título';
+$mensaje = 'Hola';
+$cabeceras = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($para, $titulo, $mensaje, $cabeceras);
+header("Location:gracias.html");
+?>
